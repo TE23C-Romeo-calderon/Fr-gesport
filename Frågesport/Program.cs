338d9ae6b8a7feca5b4e540dbecbd4efcb9e38de
Romeo Calderon <romeo.calderon@elev.ga.ntig.se>
2024-09-19 12:47:55 +0200

    Frågesport

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int point = 0;
+Console.WriteLine("Video game quiz");
+
+Console.WriteLine("Question one: What was Super Mario originally known as?");
+Console.WriteLine("a) Rescueman b) Plungerman c) Jumpman");
+
+String answer1 = Console.ReadLine();
+if (answer1 == "c")
+{
+Console.WriteLine("Correct");
+    point += 1;
+}
+else
+{
+    Console.WriteLine("Incorrect");
+}
+
+Console.WriteLine("Question two: How many Boos are there to catch in luigi's mansion 1?");
+Console.WriteLine("a) 45 b) 50 c) 62");
+
+String answer2 = Console.ReadLine();
+if (answer2 == "b")
+{
+    Console.WriteLine("Correct");
+    point += 1;
+}
+else
+{
+    Console.WriteLine ("Incorrect");
+}
+
+Console.WriteLine("Question three: What is the name of John Marston's wife in red dead redemption?");
+Console.WriteLine("a) Abby b) Abigail c) Abbeli");
+
+String answer3 = Console.ReadLine();
+if (answer3 == "b")
+{
+    Console.WriteLine("Correct");
+    point += 1;
+}
+else
+{
+    Console.WriteLine("Incorrect");
+}
+
+{
+Console.WriteLine($"You got {point} points ");
+}
+Console.ReadLine();
